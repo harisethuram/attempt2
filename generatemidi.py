0baa2a9aabe3f4acb8996d4ecd9d06deaf52b92e
Hari <31365346+harisethuram@users.noreply.github.com>
2023-06-02 17:07:37 -0700

    Update generatemidi.py

--- generatemidi.py
+++ generatemidi.py
@@ -1,13 +1,18 @@
+generatemidi.py
 from midiutil import MIDIFile
 # from preprocess import preprocess
 import torch
 
-def generate_midi(notes):
+def generate_midi(midis: 'list[torch.LongTensor[D, 4]]', name):
     """
     - takes in notes (d,4) tensor where each row is a note, col 0 is pitch, col 1 is division, col 2 is beat, col 3 is tatum
     - generates a midi file for the notes
     """
+    for i, midi in enumerate(midis):
+        process_midi(midi, f'{name}{i}')
 
+def process_midi(notes, filename):
+    print(notes.shape)
     track = 0
     channel = 0
     time = 0
@@ -16,7 +21,7 @@ def generate_midi(notes):
     midi = MIDIFile(1)
     midi.addTempo(track, time, tempo)
     for i in range(1, notes.shape[0]):
-        if notes[i, 1] == 0 and notes[i, 0] == 0: # i.e. reached padding
+        if notes[i, 1] == 0 and notes[i, 0] == 0:
             break
         pitch = notes[i, 0]
         duration = notes[i, 1]
@@ -24,9 +29,5 @@ def generate_midi(notes):
             midi.addNote(track, channel, int(notes[i, 0]), time, notes[i, 1], volume)
         time += duration
 
-        # print(time, i, int(notes[i, 2]), notes[i, 3] / notes[i, 1])
-        # beat_displacement = ((notes[i, 2] - notes[i-1, 2]) + 4) % 4
-        # time += notes[i, 2] + (notes[i, 3]-1) / notes[i, 1]
-
-    with open("test.mid", "wb") as output_file:
+    with open(f"{filename}.mid", "wb") as output_file:
         midi.writeFile(output_file)
